a0ca9172780d36b7301534e3b7a57f3f37e12d81
Jacob Enders <jacobenders1@gmail.com>
2018-12-02 15:19:28 -0500

    changed all references of .html pages to .php (hopefully all of them)

--- elections.php
+++ elections.php
@@ -7,15 +7,15 @@
 		<?php require(__DIR__ . '/navbar.html'); ?>
 		<div class="article">
 			<div class="section"><h2>Elections</h2></div>
-			<a href="election1.html"><div class="section newssection">
+			<a href="election1.php"><div class="section newssection">
 				<h3>Election of April 2018</h3>
 				<h6>Presidential</h6>
 			</div></a>
-			<a href="election2.html"><div class="section newssection">
+			<a href="election2.php"><div class="section newssection">
 				<h3>Election of May 2018</h3>
 				<h6>Midterms</h6>
 			</div></a>
-			<a href="election3.html"><div class="section newssection">
+			<a href="election3.php"><div class="section newssection">
 				<h3>Election of June 2018</h3>
 				<h6>Presidential</h6>
 			</div></a>
