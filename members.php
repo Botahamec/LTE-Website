a69ab156c09c30fd9b3d4f5786030e0e74f0c85a
Mike White <32026883+Botahamec@users.noreply.github.com>
2018-12-02 19:35:43 -0500

    Added <p> to members.php
    In the future, I want to ask all of the members for quotes to put here, but the <p> makes this look nicer, so I need it here for now.

--- members.php
+++ members.php
@@ -8,13 +8,27 @@
 		
 		<div class="article">
 			<div class="section"><h2>Meet the members of LTE!</h2></div>
-			<a href="thottie_hollie.php"><div class='section newssection'><h3>Thottie Hollie</h3></div></a><br>
-			<a href="uncle_alex.php"><div class='section newssection'><h3>Uncle Alex</h3></div></a><br>
-			<a href="romulus.php"><div class='section newssection'><h3>Romulus</h3></div></a><br>
-			<a href="orden.php"><div class='section newssection'><h3>Orden</h3></div></a><br>
-			<a href="hoop28.php"><div class='section newssection'><h3>Hoop28</h3></div></a><br>
-			<a href="computerguy15.php"><div class='section newssection'><h3>Computerguy15</h3></div></a><br>
-			<a href="chancellor_federalist.php"><div class='section newssection'><h3>Chancellor Federalist</h3></div></a><br>
+			<a href="thottie_hollie.php"><div class='section newssection'>
+				<h3>Thottie Hollie</h3>
+				<p>An intelligent thot</p>
+				</div></a>
+			<a href="uncle_alex.php"><div class='section newssection'>
+				<h3>Uncle Alex</h3>
+				<p>The uncle himself</p>
+				</div></a>
+			<a href="romulus.php"><div class='section newssection'>
+				<h3>Romulus</h3>
+				<p>The big bad Rom!</p>
+				</div></a>
+			<a href="orden.php"><div class='section newssection'>
+				<h3>Orden</h3>
+				<p>Returns from Hogwarts</p>
+				</div></a>
+			<a href="hoop28.php"><div class='section newssection'>
+				<h3>Hoop28</h3>
+				</div></a>
+			<a href="computerguy15.php"><div class='section newssection'><h3>Computerguy15</h3></div></a>
+			<a href="chancellor_federalist.php"><div class='section newssection'><h3>Chancellor Federalist</h3></div></a>
 		</div>
 	</body>
 </html>
