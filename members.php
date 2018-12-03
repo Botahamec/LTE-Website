d5ebf95d20f755e91729ac7c8937a9f2a58bdd54
Mike White <32026883+Botahamec@users.noreply.github.com>
2018-12-02 19:23:03 -0500

    Fixed the members section
    From now on, whenever we have links, use <div class='section newssection'> for the full button. Use <h3> for the title. Use <h6> for the subtitle. Use <p> for the description. All the CSS will handle itself. Thanks

--- members.php
+++ members.php
@@ -19,13 +19,13 @@
 		<div class="article">
 			<div class="section"><h2>Meet the members of LTE!</h2></div>
 			<div class="section">
-				<a href="thottie_hollie.php">Thottie Hollie</a><br>
-				<a href="uncle_alex.php">Uncle Alex</a><br>
-				<a href="romulus.php">Romulus</a><br>
-				<a href="orden.php">Orden</a><br>
-				<a href="hoop28.php">Hoop28</a><br>
-				<a href="computerguy15.php">Computerguy15</a><br>
-				<a href="chancellor_federalist.php">Chancellor Federalist</a><br>
+				<a href="thottie_hollie.php"><div class='section newssection'><h3>Thottie Hollie</h3></div></a><br>
+				<a href="uncle_alex.php"><div class='section newssection'><h3>Uncle Alex</h3></div></a><br>
+				<a href="romulus.php"><div class='section newssection'><h3>Romulus</h3></div></a><br>
+				<a href="orden.php"><div class='section newssection'><h3>Orden</h3></div></a><br>
+				<a href="hoop28.php"><div class='section newssection'><h3>Hoop28</h3></div></a><br>
+				<a href="computerguy15.php"><div class='section newssection'><h3>Computerguy15</h3></div></a><br>
+				<a href="chancellor_federalist.php"><div class='section newssection'><h3>Chancellor Federalist</h3></div></a><br>
 			</div>
 		</div>
 	</body>
