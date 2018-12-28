8dc0c97bf1e3dce1a0ffd24c829e47e267d5fe92
Jacob Enders <jacob@Jacob-Laptop>
2018-12-28 13:28:49 -0500

    added alligators page

---
+++ alligator.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+	<head>
+		<title>LTE Elections | Thottie Hollie</title>
+		<?php require(__DIR__ . '/metadata.html'); ?>
+	</head>
+	<body>
+		<?php require(__DIR__ . '/navbar.html'); ?>
+		
+		<div class="article">
+			<div class="section"><h2>Alligator!</h2></div>
+			<div class="section">
+				<h3>Who is Alligator?</h3>
+				<p>
+					A progressive libertarian.
+				</p>
+			</div>
+		</div>
+	</body>
+</html>
