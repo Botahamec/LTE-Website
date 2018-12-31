30b632a9cf8c177a50fa3e2a3b400a28bce820ab
Jacob Enders <jacob@Jacob-Laptop>
2018-12-31 12:52:13 -0500

    usa

---
+++ unitedstatesofamerica.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+	<head>
+		<title>United States of America</title>
+		<?php require(__DIR__ . '/metadata.html'); ?>
+	</head>
+	<body>
+		<?php require(__DIR__ . '/navbar.html'); ?>
+		
+		<div class="article">
+			<div class="section"><h2>United States of America</h2></div>
+			<div class="section">
+				<h3>What is the United States of America</h3>
+				<p>
+					A wanna be LTE server... </br>
+					Republicans are all neocons </br>
+				</p>
+			</div>
+		</div>
+	</body>
+</html>
