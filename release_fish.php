5ba839d856189d3ac8e47713463b53a1ca2488b1
Jacob Enders <jacob@Jacob-Laptop>
2019-01-01 11:59:44 -0500

    help

---
+++ release_fish.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+	<head>
+		<title>FISH MUST BE RELEASED</title>
+		<?php require(__DIR__ . '/metadata.html'); ?>
+	</head>
+	<body>
+		<?php require(__DIR__ . '/navbar.html'); ?>
+		
+		<div class="article">
+			<div class="section"><h2>Release Fish!</h2></div>
+			<div class="section">
+				<h3>I AM A HIGH RANKING DEVELOPER?</h3>
+				<p>
+					I DEMAND TO BE LET OUT OF GULAG
+				</p>
+			</div>
+		</div>
+	</body>
+</html>
